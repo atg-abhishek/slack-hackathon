195d109a7a70649012cc32cb52b8ea0a620cc1f5
Mathieu Wang <mathieu.sy.wang@gmail.com>
2016-06-04 17:01:19 -0400

    Save different channel histories to different json files

--- bot/parserbot.py
+++ bot/parserbot.py
@@ -4,29 +4,19 @@ import random
 from slackclient import SlackClient
 import time
 import yaml
+import json
 
 config = yaml.load(file('parserbot.conf', 'r'))
 api_key = config["SLACK_API_TOKEN"]
+offices = config["OFFICES"]
+
 client = SlackClient(api_key)
 
 if client.rtm_connect():
-    print("connected")
-    # while True:
-    last_read = client.rtm_read()
-    channel1_id = client.server.channels.find("london").id
-
-    history = client.api_call("channels.history", channel=channel1_id)
-    print(history)
-
-    if last_read:
-        try:
-            parsed = last_read[0]['text']
-            #reply to channel message was found in.
-            message_channel = last_read[0]['channel']
-            # print(message_channel)
-            if parsed and 'food:' in parsed:
-                choice = random.choice(['hamburger', 'pizza'])
-                client.rtm_send_message(message_channel,
-                                        'Today you\'ll eat %s.' % choice)
-        except:
-            pass
+  print("connected")
+  for office in offices:
+    print(office)
+    channel_id = client.server.channels.find(office).id
+    history = client.api_call("channels.history", channel=channel_id)
+    with open('{}.json'.format(office), 'w') as outfile:
+      json.dump(json.loads(history), outfile)
