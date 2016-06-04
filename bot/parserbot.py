2ef5fc1304a93c3181a7f6949f841e5bea4784ae
Mathieu Wang <mathieu.sy.wang@gmail.com>
2016-06-04 15:52:56 -0400

    Get history of a specific channel

--- bot/parserbot.py
+++ bot/parserbot.py
@@ -11,19 +11,22 @@ client = SlackClient(api_key)
 
 if client.rtm_connect():
     print("connected")
-    while True:
-        last_read = client.rtm_read()
-        # print(last_read)
-        if last_read:
-            try:
-                parsed = last_read[0]['text']
-                #reply to channel message was found in.
-                message_channel = last_read[0]['channel']
-                # print(message_channel)
-                if parsed and 'food:' in parsed:
-                    choice = random.choice(['hamburger', 'pizza'])
-                    client.rtm_send_message(message_channel,
-                                            'Today you\'ll eat %s.' % choice)
-            except:
-                pass
-        time.sleep(1)
+    # while True:
+    last_read = client.rtm_read()
+    channel1_id = client.server.channels.find("london").id
+
+    history = client.api_call("channels.history", channel=channel1_id)
+    print(history)
+
+    if last_read:
+        try:
+            parsed = last_read[0]['text']
+            #reply to channel message was found in.
+            message_channel = last_read[0]['channel']
+            # print(message_channel)
+            if parsed and 'food:' in parsed:
+                choice = random.choice(['hamburger', 'pizza'])
+                client.rtm_send_message(message_channel,
+                                        'Today you\'ll eat %s.' % choice)
+        except:
+            pass
